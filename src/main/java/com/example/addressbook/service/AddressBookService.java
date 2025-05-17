7ac489115dc7576f446c443c844896b06fe7566b
Yogesh <yogeshwarransk@gmail.com>
2025-05-17 10:00:00 +0530

    UC5 : Implement in-memory CRUD logic

---
+++ src/main/java/com/example/addressbook/service/AddressBookService.java
@@ -0,0 +1,47 @@
+package com.example.AddressBook.service;
+
+import com.example.AddressBook.dto.AddressDTO;
+import com.example.AddressBook.model.AddressBookData;
+import com.example.AddressBook.repository.AddressBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressBookService implements IAddressBookService {
+
+    @Autowired
+    private AddressBookRepository repository;
+
+    @Override
+    public List<AddressBookData> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public AddressBookData getById(int id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("ID not found"));
+    }
+
+    @Override
+    public AddressBookData create(AddressDTO dto) {
+        AddressBookData data = new AddressBookData();
+        data.setName(dto.name);
+        data.setPhone(dto.phone);
+        return repository.save(data);
+    }
+
+    @Override
+    public AddressBookData update(int id, AddressDTO dto) {
+        AddressBookData data = getById(id);
+        data.setName(dto.name);
+        data.setPhone(dto.phone);
+        return repository.save(data);
+    }
+
+    @Override
+    public void delete(int id) {
+        repository.deleteById(id);
+    }
+}
